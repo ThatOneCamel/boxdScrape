ec075af86eca6082b69cfe283725b00eb3a720e4
Cam <thomascameront@gmail.com>
2023-04-17 10:57:31 -0500

    Init Separate Repo

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import request
+import urllib.request
+import re
+from requests_html import HTMLSession
+
+app = Flask(__name__)
+
+#Makes all responses plaintext
+@app.after_request
+def treat_as_plain_text(response):
+    response.headers["content-type"] = "text/plain"
+    return response
+
+@app.route('/')
+def getList():
+    url = request.args.get("listurl")
+    return getPage(url)
+
+@app.route('/sample')
+def getSampleList():
+    return getPage("https://letterboxd.com/crew/list/101-movies-that-missingmoviesorg-would-love/page/2/")
+
+url = "https://letterboxd.com/crew/list/101-movies-that-missingmoviesorg-would-love/"
+
+session = HTMLSession()
+resp = session.get(url)
+
+def getPage(url):
+    try:
+        response = urllib.request.urlopen(url)
+        print("Established connection")
+        data = response.read()
+        page = data.decode("utf8")
+
+
+        #Pure html solution.
+        films = list(re.findall("alt=\"(.*?)\"", page))
+        films.pop(0)
+        i = 1
+        listStr = ""
+        for movie in films:
+            print("[{}] {}".format(i, movie))
+            listStr += movie + "\n"
+            i += 1
+    
+        response.close()
+        return listStr
+        
+    except Exception as e:
+        print(e)
+        print("Failed to retrieve info from webpage")
+        return set()
+
+myStr = getPage(url)
---
+++ pyAPI.py
@@ -0,0 +1,43 @@
+import urllib.request
+import re
+from requests_html import HTMLSession
+
+url = "https://letterboxd.com/crew/list/101-movies-that-missingmoviesorg-would-love/page/2/"
+
+session = HTMLSession()
+resp = session.get(url)
+ 
+#resp.html.render(scrolldown=10, timeout=30)
+titles = resp.html.find('.numbered-list-item')
+for title in titles:
+	print(title.text)
+
+f = open("htmlcontent.txt", "w")
+f.write(resp.html.html)
+f.close()
+
+def getPage(url):
+	try:
+		response = urllib.request.urlopen(url)
+		print("Established connection")
+		data = response.read()
+		page = data.decode("utf8")
+
+
+		#Pure html solution.
+		x = list(re.findall("alt=\"(.*?)\"", page))
+		x.pop(0)
+		i = 1
+		for item in x:
+			print("[{}] {}".format(i, item))
+			i += 1
+	
+		response.close()
+		return x
+		
+	except Exception as e:
+		print(e)
+		print("Failed to retrieve info from webpage")
+		return set()
+
+myStr = getPage(url)
